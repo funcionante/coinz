006a5bd359584922177ac33a334c5a03053384b9
Leonardo Oliveira <leonardooliveira@ua.pt>
2016-07-01 20:16:59 +0100

    Added some screenshots.

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -19,6 +19,48 @@
 
             <div class="box box-default">
                 <div class="box-header with-border">
+                    <h3 class="box-title">Screenshots</h3>
+                </div>
+                <div class="box-body">
+                    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                            @for ($i = 1; $i < 8; $i++)
+                                <li data-target="#carousel-example-generic" data-slide-to="{{ $i }}"></li>
+                            @endfor
+                        </ol>
+
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+
+                            <div class="item active">
+                                <img src="media/screenshots/1.jpg">
+                            </div>
+
+                            @for ($i = 2; $i < 9; $i++)
+                                <div class="item">
+                                    <img src="media/screenshots/{{ $i }}.jpg">
+                                </div>
+                            @endfor
+                        </div>
+
+                        <!-- Controls -->
+                        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                            <span class="sr-only">Anterior</span>
+                        </a>
+                        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                            <span class="sr-only">Seguinte</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="box box-default">
+                <div class="box-header with-border">
                     <h3 class="box-title">Autor</h3>
                 </div>
                 <div class="box-body">
